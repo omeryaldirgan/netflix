2e3df16d4b5553dfec4fb14a8a05ee0e3bf738f7
omeryaldirgan <omeryaldirgan@hotmail.com>
2021-02-26 22:27:31 +0300

    Update reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,5 +1,6 @@
 const reportWebVitals = onPerfEntry => {
 
+
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
@@ -9,6 +10,7 @@ const reportWebVitals = onPerfEntry => {
       getTTFB(onPerfEntry);
     });
   }
+  
 };
 
 export default reportWebVitals;
